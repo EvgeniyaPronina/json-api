c014e612dd44626a24236cd01213bd0c8b28d1dc
Evgeniya Pronina <rezinkina@list.ru>
2016-03-21 13:29:39 +0600

    second version

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
 <?php
-include_once 'init.php';
-$route = explode('/', $_SERVER['REQUEST_URI']);
-$table = filter_var($_POST['table'], FILTER_SANITIZE_STRING);
-$id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);//id поля таблицы
-$sessionId = filter_var($_POST['sessionId'], FILTER_SANITIZE_NUMBER_INT);//id сессии, на которую нужно записаться
-$userEmail = filter_var($_POST['userEmail'], FILTER_SANITIZE_EMAIL);
-$newsTitle = filter_var($_POST['newsTitle'], FILTER_SANITIZE_STRING);
-$newsMessage = filter_var($_POST['newsMessage'], FILTER_SANITIZE_STRING);
+spl_autoload_register(function($className) {
+    require_once 'Classes/'.$className.'.php';
+});
 
-switch ($route[4]) {
-    case 'Table':
-        $current_table = $table;
-        $obj = new $current_table;
-        $answer = $obj ->Table($current_table);
-        break;
-    case 'SessionSubscribe':
-        $obj = new Session;
-        $answer = $obj ->Subscribe($sessionId, $userEmail);
-        break;
-    case 'PostNews':
-        $obj = new News;
-        $answer = $obj ->Post($userEmail, $newsTitle, $newsMessage);
-        break;
-    default:
-        $mes = array(
-            'status' => "error",
-            'message' => "Ошибка в запросе");
-        $jsonOutput = json_encode($mes);
-        return($jsonOutput);
-        break;
+$route = explode('/', $_SERVER['REQUEST_URI']);
+$controllerName = $route[4];
+
+if (method_exists('Controllers', $controllerName)) {
+    $answear = (new Controllers())->$controllerName();
+} else {
+    $answear = [
+        'status' => "error",
+        'message' => "Ошибка в запросе"];
 }
+$mes = new Message($answear);
+return $mes->jsonMessage();
